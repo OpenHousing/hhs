88535122555e3ba32bf498cff3c75cd3026aeb81
Chris Alfano <chris@jarv.us>
2018-01-14 20:10:55 -0500

    Add load-clients script to populate postgres

---
+++ load-clients.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+// load config from .env into process.env
+require('dotenv').config();
+
+
+// connect to database
+const db = require('./db');
+
+
+// read clients
+(async () => {
+    // load clients
+    const clients = JSON.parse(require('fs').readFileSync('./static/client-enrollments.json'));
+
+    // create table
+    await db.Client.sync({force: true});
+
+    // write clients
+    try {
+        await db.Client.bulkCreate(clients.map(client => {
+            client.id = client.hmisID;
+            client.dedup_client_id = client.dedupClientId;
+            client.first_name = client.firstName;
+            client.last_name = client.lastName;
+            client.disabling_condition = Boolean(client.disabling_condition);
+
+            return client;
+        }));
+    } catch (err) {
+        console.error('failed to bulk create clients:', err);
+    }
+
+    process.exit(0);
+})();
